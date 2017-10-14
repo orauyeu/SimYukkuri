3109cb9a6afbc1e9e7f2bbe2553930c35cdac526
kotorinn <31925542+kotorinn@users.noreply.github.com>
2017-10-14 18:18:47 +0900

    YukkuriStatをYukkuriStatsに

---
+++ subprojects/simyukkuri/src/main/kotlin/simyukkuri/gameobject/yukkuri/statistic/YukkuriStats.kt
@@ -0,0 +1,18 @@
+package simyukkuri.gameobject.yukkuri.statistic
+
+import messageutil.Condition
+import simyukkuri.gameobject.yukkuri.event.IndividualEvent
+import simyukkuri.gameobject.yukkuri.statistic.statistics.*
+
+/** ゆっくりのステータスのインターフェース */
+interface YukkuriStats :
+        Damage, Emotion, Family, Fullness, Growth, Message, MiscStat, Movement, Poo, Pregnancy, Sleep, Sukkiri, Yukabi {
+    /** このゆっくりのメッセージのリスト */
+    val msgList: MessagePicker
+
+    var event: IndividualEvent
+
+    val messageCondition: Condition
+
+    override fun update()
+}
