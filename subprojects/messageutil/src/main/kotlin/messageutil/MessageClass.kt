6e10a510e9ee8777e294e77e27fd4f41095dec39
kotorinn <31925542+kotorinn@users.noreply.github.com>
2017-10-14 13:45:48 +0900

    * メッセージを取り出す機能をリソースパッケージからゆっくり側に移した * それに伴い, `Messages`クラスはマップを持つだけになったので ただのグローバル変数を持つファイルに変更

--- subprojects/messageutil/src/main/kotlin/messageutil/MessageClass.kt
+++ subprojects/messageutil/src/main/kotlin/messageutil/MessageClass.kt
@@ -6,55 +6,11 @@ import java.nio.file.Path
 /** メッセージを保管するクラスの名前 */
 val messageClassName = "Messages"
 
-/* 編集用
-abstract class Messages(name: String) {
-    private val msgMap: Map<String, Map<Condition, List<String>>>
-
-    init {
-        val tmpMsgMap = linkedMapOf<String, MutableMap<Condition, MutableList<String>>>()
-        val rawMsgMap = @Suppress("UNCHECKED_CAST") (Root::class.java.getResourceAsStream("messages/$name.yml").use { myYaml.load(it) } as MessageData)
-        for ((key, condToMsgs) in rawMsgMap) {
-            val tmpCondToMsgs = linkedMapOf<Condition, MutableList<String>>()
-            tmpMsgMap.put(key, tmpCondToMsgs)
-            for ((rawStats, msgs) in condToMsgs) {
-                if (rawStats.growth == Growth.BABY_OR_CHILD) {
-                    tmpCondToMsgs.getOrPut(rawStats.copy(growth = Growth.BABY)) { mutableListOf() }.addAll(msgs)
-                    tmpCondToMsgs.getOrPut(rawStats.copy(growth = Growth.CHILD)) { mutableListOf() }.addAll(msgs)
-                } else
-                    tmpCondToMsgs.getOrPut(rawStats) { mutableListOf() }.addAll(msgs)
-            }
-        }
-        msgMap = tmpMsgMap
-    }
-}
-*/
-
 // TODO: ダメージを受けている版のメッセージがない場合, 通常版のメッセージで代用するなど欠けを補う.
-/** メッセージデータに対応する, セリフ毎にプロパティを持つクラスの文字列を生成する. */
-fun messageDataToPojoString(msgData: Map<String, Any?>): String = buildString {
-    appendln("abstract class Messages(name: String) {\n" +
-            "    private val msgMap: Map<String, Map<Condition, List<String>>>\n" +
-            "\n" +
-            "    init {\n" +
-            "        val tmpMsgMap = linkedMapOf<String, MutableMap<Condition, MutableList<String>>>()\n" +
-            "        val rawMsgMap = @Suppress(\"UNCHECKED_CAST\") (Root::class.java.getResourceAsStream(\"messages/\$name.yml\").use { myYaml.load(it) } as MessageData)\n" +
-            "        for ((key, condToMsgs) in rawMsgMap) {\n" +
-            "            val tmpCondToMsgs = linkedMapOf<Condition, MutableList<String>>()\n" +
-            "            tmpMsgMap.put(key, tmpCondToMsgs)\n" +
-            "            for ((rawStats, msgs) in condToMsgs) {\n" +
-            "                if (rawStats.growth == Growth.BABY_OR_CHILD) {\n" +
-            "                    tmpCondToMsgs.getOrPut(rawStats.copy(growth = Growth.BABY)) { mutableListOf() }.addAll(msgs)\n" +
-            "                    tmpCondToMsgs.getOrPut(rawStats.copy(growth = Growth.CHILD)) { mutableListOf() }.addAll(msgs)\n" +
-            "                } else\n" +
-            "                    tmpCondToMsgs.getOrPut(rawStats) { mutableListOf() }.addAll(msgs)\n" +
-            "            }\n" +
-            "        }\n" +
-            "        msgMap = tmpMsgMap\n" +
-            "    }" +
-            "\n")
+/** ゆっくりの発言パーツのメッセージデータのプロパティ部分を生成する. */
+fun messageDataToProperty(msgData: Map<String, Any?>): String = buildString {
     for ((messageName) in msgData)
-        appendln("    fun $messageName(cond: Condition): String? = msgMap[\"$messageName\"]?.get(stat)?.randomElement()")
-    append("}")
+        appendln("val $messageName: String = messageData[\"$messageName\"]?.get(self.messageCondition)?.randomElement()")
 }
 
 /** 指定されたセリフデータからMessagesクラスのソースファイルを作成し, [writeDir]に保存する. */
@@ -64,7 +20,7 @@ fun writeClassSource(writeDir: Path, vararg messagePaths: Path) {
             0 -> ""
             1 -> Files.newInputStream(messagePaths[0])
                 .use { @Suppress("UNCHECKED_CAST") (myYaml.load(it) as LinkedHashMap<String, Map<Condition, List<String>>>) }
-                .let { messageDataToPojoString(it) }
+                .let { messageDataToProperty(it) }
             else -> run {
                 val messageData = Files.newInputStream(messagePaths[0])
                     .use { @Suppress("UNCHECKED_CAST") (myYaml.load(it) as LinkedHashMap<String, Map<Condition, List<String>>>) }
@@ -73,7 +29,7 @@ fun writeClassSource(writeDir: Path, vararg messagePaths: Path) {
                         .use { @Suppress("UNCHECKED_CAST") (myYaml.load(it) as Map<String, Map<Condition, List<String>>>) }
                         .let { messageData.putAll(it) }
                 }
-                messageDataToPojoString(messageData)
+                messageDataToProperty(messageData)
             }
         }
     }
--- subprojects/simyukkuri/src/main/kotlin/simyukkuri/gameobject/yukkuri/statistic/statistics/MessageImpl.kt
+++ subprojects/simyukkuri/src/main/kotlin/simyukkuri/gameobject/yukkuri/statistic/statistics/MessageImpl.kt
@@ -2,7 +2,7 @@ package simyukkuri.gameobject.yukkuri.statistic.statistics
 
 import simyukkuri.Time
 
-class MessageImpl : Message {
+class MessageImpl(private val messageData: MessageData) : Message {
     override var message: String? = null
 
     /** メッセージ表示を終了するまでの残り時間 */
@@ -24,4 +24,46 @@ class MessageImpl : Message {
             message = null
         }
     }
+
+    val findsFood: String = messageData["findsFood"]?.get(self.messageCondition)?.randomElement()
+    val wantsFood: String = messageData["wantsFood"]?.get(self.messageCondition)?.randomElement()
+    val sexuallyExcited: String = messageData["sexuallyExcited"]?.get(self.messageCondition)?.randomElement()
+    val relaxed: String = messageData["relaxed"]?.get(self.messageCondition)?.randomElement()
+    val furifuri: String = messageData["furifuri"]?.get(self.messageCondition)?.randomElement()
+    val wakesUp: String = messageData["wakesUp"]?.get(self.messageCondition)?.randomElement()
+    val sleeping: String = messageData["sleeping"]?.get(self.messageCondition)?.randomElement()
+    val screams: String = messageData["screams"]?.get(self.messageCondition)?.randomElement()
+    val scared: String = messageData["scared"]?.get(self.messageCondition)?.randomElement()
+    val scaredAtRaper: String = messageData["scaredAtRaper"]?.get(self.messageCondition)?.randomElement()
+    val alarmed: String = messageData["alarmed"]?.get(self.messageCondition)?.randomElement()
+    val killedInstantly: String = messageData["killedInstantly"]?.get(self.messageCondition)?.randomElement()
+    val dies: String = messageData["dies"]?.get(self.messageCondition)?.randomElement()
+    val eating: String = messageData["eating"]?.get(self.messageCondition)?.randomElement()
+    val eatingPoo: String = messageData["eatingPoo"]?.get(self.messageCondition)?.randomElement()
+    val hasEatenBitter: String = messageData["hasEatenBitter"]?.get(self.messageCondition)?.randomElement()
+    val getsFull: String = messageData["getsFull"]?.get(self.messageCondition)?.randomElement()
+    val takesSweets: String = messageData["takesSweets"]?.get(self.messageCondition)?.randomElement()
+    val hasSukkiried: String = messageData["hasSukkiried"]?.get(self.messageCondition)?.randomElement()
+    val born: String = messageData["born"]?.get(self.messageCondition)?.randomElement()
+    val startsToPoop: String = messageData["startsToPoop"]?.get(self.messageCondition)?.randomElement()
+    val needsToPoop: String = messageData["needsToPoop"]?.get(self.messageCondition)?.randomElement()
+    val hasPooped: String = messageData["hasPooped"]?.get(self.messageCondition)?.randomElement()
+    val leaksPoo: String = messageData["leaksPoo"]?.get(self.messageCondition)?.randomElement()
+    val surisuri: String = messageData["surisuri"]?.get(self.messageCondition)?.randomElement()
+    val peropero: String = messageData["peropero"]?.get(self.messageCondition)?.randomElement()
+    val bearing: String = messageData["bearing"]?.get(self.messageCondition)?.randomElement()
+    val lamentsForAbortingBaby: String = messageData["lamentsForAbortingBaby"]?.get(self.messageCondition)?.randomElement()
+    val complainsAboutPoo: String = messageData["complainsAboutPoo"]?.get(self.messageCondition)?.randomElement()
+    val hungry: String = messageData["hungry"]?.get(self.messageCondition)?.randomElement()
+    val losesOkazari: String = messageData["losesOkazari"]?.get(self.messageCondition)?.randomElement()
+    val despisesYukkuriHavingNoOkazari: String = messageData["despisesYukkuriHavingNoOkazari"]?.get(self.messageCondition)?.randomElement()
+    val saysLikeFlying: String = messageData["saysLikeFlying"]?.get(self.messageCondition)?.randomElement()
+    val childDies: String = messageData["childDies"]?.get(self.messageCondition)?.randomElement()
+    val partnerDies: String = messageData["partnerDies"]?.get(self.messageCondition)?.randomElement()
+    val elderSisterDies: String = messageData["elderSisterDies"]?.get(self.messageCondition)?.randomElement()
+    val youngerSisterDies: String = messageData["youngerSisterDies"]?.get(self.messageCondition)?.randomElement()
+    val blockedByWall: String = messageData["blockedByWall"]?.get(self.messageCondition)?.randomElement()
+    val getsTreasure: String = messageData["getsTreasure"]?.get(self.messageCondition)?.randomElement()
+    val treasureTaken: String = messageData["treasureTaken"]?.get(self.messageCondition)?.randomElement()
+    val nobinobi: String = messageData["nobinobi"]?.get(self.messageCondition)?.randomElement()
 }
